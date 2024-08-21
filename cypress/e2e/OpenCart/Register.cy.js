100ea81eceeddafe1e4bcc95f6a7391720b3699b
Ahmad <ahmadalam15401@gmail.com>
2024-08-20 23:34:05 -0700

    Add code

--- cypress/e2e/OpenCart/Register.cy.js
+++ cypress/e2e/OpenCart/Register.cy.js
@@ -4,6 +4,8 @@
 describe('Register', () => {
     it('Register to OpenCart', () => {
       cy.visit('https://www.opencart.com/')
+      cy.contains('a', 'Login').click({force: true})
+      cy.wait(3000)
     })
   })
 
