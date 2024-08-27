87734045a1ed13856a5d5833e418d1cf8f147ffe
Ahmad <ahmadalam15401@gmail.com>
2024-08-27 03:12:15 -0700

    new changes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,4 +23,4 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import 'cypress-mochawesome-reporter/register';
+// import 'cypress-mochawesome-reporter/register';
